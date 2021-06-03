74e726f7de93f5b19b3282fe91eec8720cb71cf9
ceciliamm <cecilia@badgermapping.com>
2021-06-03 14:01:30 +0200

    Milestone: new task form page

--- lib/pages/add_task_form.dart
+++ lib/pages/add_task_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MeasuringUnit {
   String id;
@@ -54,62 +55,101 @@ class _AddTaskFormState extends State<AddTaskForm> {
         appBar: AppBar(
           title: Text("New Task"),
         ),
-        body: Form(
-          key: _formKey,
-          child: Center(
-            child: Column(
-              children: [
-                CupertinoFormSection(children: [
-                  CupertinoTextFormFieldRow(
-                    placeholder: 'Task title ',
-                    controller: _taskTitle,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a task title';
-                      }
-                      return null;
-                    },
-                  ),
-                  CupertinoTextFormFieldRow(
-                    maxLines: 4,
-                    placeholder: 'Enter a description here',
-                    controller: _description,
-                  )
-                ]),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: 0.0, top: 15.0, right: 0.0, bottom: 15.0),
-                  child: CupertinoFormSection(children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Flexible(
-                            child: Padding(
-                              padding: const EdgeInsets.all(6.0),
-                                  child: Text('Measured in',
-                                      style: TextStyle(
-                                          fontStyle: FontStyle.italic,
-                                          fontSize: 17
-                                      )
-                                  ),
-                            )
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: buildMeasuringUnitsDropdownButtonFormField(),
-                          ),
-                        ),
-                      ],
-                    )
-                  ]),
+        body: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Center(
+                child: Column(
+                  children: [
+                    CupertinoFormSection(children: [
+                      CupertinoTextFormFieldRow(
+                        placeholder: 'Task title ',
+                        controller: _taskTitle,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a task title';
+                          }
+                          return null;
+                        },
+                      ),
+                      CupertinoTextFormFieldRow(
+                        maxLines: 4,
+                        placeholder: 'Enter a description here',
+                        controller: _description,
+                      )
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 0.0, top: 15.0, right: 0.0, bottom: 15.0),
+                      child: CupertinoFormSection(children: [
+                        measuredInRow(),
+                        totalEffortRow()
+                      ]),
+                    ),
+                  ],
                 ),
-              ],
+              ),
+            ),
+            CupertinoButton.filled(
+              onPressed: () => {},
+              child: const Text('Save'),
+            )
+          ],
+        ),
+    );
+  }
+
+  Row measuredInRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                  child: Text('Measured in',
+                    style: TextStyle(fontStyle: FontStyle.italic, fontSize: 17)),
+              )),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: buildMeasuringUnitsDropdownButtonFormField(),
             ),
           ),
-        ));
-  }
+        ],
+      );
+
+  Row totalEffortRow() => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text('Total effort',
+                    style: TextStyle(fontStyle: FontStyle.italic, fontSize: 17)),
+          )),
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new CupertinoTextFormFieldRow(
+                    placeholder: 'Quantity',
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ]),
+              )),
+          Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text('${selectedUnit.name}',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    color: Colors.grey)),
+          ))
+        ],
+      );
 
   DropdownButtonFormField<MeasuringUnit>
       buildMeasuringUnitsDropdownButtonFormField() =>
